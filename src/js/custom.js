57c7297e76c6f7f3c37fb3467d819a83237ff910
VikashSaharan1 <vikashpkl@gmail.com>
2020-01-16 01:07:50 +0530

    add cart page code, javascript add to cart functionality, updatenotfication

--- src/js/custom.js
+++ src/js/custom.js
@@ -5,28 +5,48 @@ var adobeEcommerce  = (function(functionName, baseObj) {
         baseObj = baseObj || window;
         var sortByArr;
         var responseJson;
+        this.cart = [];
         const srcUrl = 'https://api.myjson.com/bins/qzuzi/';
         removeActiveClass = () => {
             Array.from(sortByArr).forEach((element)  => {
                 element.classList.remove('active');
             });
         };
-        sortBy = (event, flag) => {
+        sortBy = (event, isClicked) => {
             this.removeActiveClass();
             event.srcElement.classList.add("active");
+            if(event.srcElement.id == "hl") {
+                this.responseJson = this.sortByHighLow();
+            } else  if(event.srcElement.id == "lh") {
+                this.responseJson = this.sortByLowHigh();
+            } else  if(event.srcElement.id == "dis"){
+                this.responseJson = this.sortByDiscount();
+            }    
+            this.displayItemList();
+        };
+        sortByHighLow = () => {
+            return this.responseJson.sort((a, b) => b.price - a.price);
+        };
+        sortByLowHigh = () => {
+            return this.responseJson.sort((a, b) => a.price - b.price);
+        };
+        sortByDiscount = () => {
+            return this.responseJson.sort((a, b) => b.discount - a.discount);
         };
         caluclateAfterDiscountPrice = (mrp, discount) => {
             return (mrp - Math.round(mrp/100 * discount));
         }; 
         displayItemList = () => {
+
             let element, Obj, newprice, html = "";
             let objItemContainer = document.getElementsByClassName("items-container")[0];
+            objItemContainer.innerHTML = '';
             for(element in this.responseJson) {
                 Obj = this.responseJson[element];
                 newprice = this.caluclateAfterDiscountPrice(Number.parseFloat(Obj.price), Number.parseFloat(Obj.discount));
                 // Obj.img_url is exiting in data but it's take time to loading
 
-                html = `<div class="item" >
+                html = `<div class="item" id="${Obj.id}">
                 <div class="image"><img src="assets/image/earphone.jfif"/></div>
                 <div class="name">${Obj.name}</div>
                 <div class="price"><span class="newprice">${newprice}</span><span class="mrp">${Obj.price}</span><span class="discount-percent">${Obj.discount}% off</span></div>
@@ -52,17 +72,54 @@ var adobeEcommerce  = (function(functionName, baseObj) {
                 xmlhttp.send();
             });
         };
+        addtoCartbtn = (event) => {
+            
+            let id = event.srcElement.parentNode.parentNode.getAttribute("id");
+            let isExistObj = Boolean(this.cart.length && this.cart.filter((a) => { return a.id == id}));
+            let Obj = this.responseJson.filter((a) => {return a.id == id; });
+            this.cart.push(Obj);
+            this.updateNotifyIcon();
+
+        };
+        updateNotifyIcon = () => {
+            let cartItemsCount = this.cart.length;
+            let notifyIcon = document.getElementById("notify-icon");
+            if(cartItemsCount > 0) {
+                notifyIcon.innerText = cartItemsCount;
+                notifyIcon.classList.remove("d-none");
+            } else {
+                notifyIcon.classList.add("d-none");
+            }
+        };
+        removeCartItem = (event) => {
+                event.target.parentElement.remove();
+                this.updatreCartTotal
+        };
+        updatreCartTotal = () => {
+                
+        };
         init = () => {
+            this.updateNotifyIcon();
             sortByArr = document.getElementsByClassName("sort-by-opt");
+            //sortByArr.addEventListener('click', this.sortBy.bind(this), false);
             Array.from(sortByArr).forEach((element) => {
                 element.addEventListener('click', this.sortBy.bind(this), false);
             });
+            
             this.getJsonFromSRC(srcUrl).then(data => {
                 this.responseJson = data;
-                this.displayItemList();
+                sortByArr[0].click();
+                let Obj = document.getElementsByClassName("btn-addtocart");
+                Array.from(Obj).forEach((element) => {
+                    element.addEventListener('click', this.addtoCartbtn.bind(element), false);
+                });
             }).catch(error => {
                 console.log(error);
             });
+            /*let removeCartObj = document.getElementsByClassName("btn-removetocart");
+            Array.from(removeCartObj).forEach((element) => {
+                element.addEventListener("click", this.removeCartItem.bind(this), false);
+            });*/ 
         };
         return init;
 })("docReady", window);
